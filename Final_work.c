2e043be3dae137b5b1f094fadc22cadd6e7fb633
Kenduffy5275 <81328337+Kenduffy5275@users.noreply.github.com>
2021-03-24 15:45:16 -0400

    Add files via upload

---
+++ Final_work.c
@@ -0,0 +1,18 @@
+///Contact Management System///
+
+//1. Request the Admin username and password(string of at least 8 characters)
+//2. Be able to change username and password
+//3. Add users to the CMS when logged in as admin
+//4. Remove users
+//5. Admin's should be able to
+	//a. add info to include first, last, cell #, work #, fax #, email, gender, age
+	//b. remove records.
+	//c. Edit records
+	//d. Search through contacts based on anything listed in a.
+//6. The CMS should be user-friendly
+	//a. gives a warning if the user tries to put contact information with a name that already exists
+	//b. Has a welcome page
+	//c. has a menu for all functions
+	//d. provides reports in a table 
+	//e. has an exit function
+//7. OPTIONAL--All information is ciphered
